d9203bc775196c8a6771ad404cea60d7419e896b
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2022-03-28 05:44:34 -0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+is_cool = False
+is_warm = True
+
+if is_cool:
+    print("Its a cold day")
+    print("Go to the swimming pool ")
+
+elif is_warm:
+    print("Its a pretty warm day ")
+    print("Wear simple clothes ")
+
+else:
+    print("Its neither a warm day nor a cold day. it is beautiful day ")
+
+print("Enjoy your cold day with a cup of coffee")
